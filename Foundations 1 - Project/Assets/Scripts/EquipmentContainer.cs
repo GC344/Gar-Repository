32f591f8e9189c4c70338c36bcb27f5532083460
GC344 <56491943+GC344@users.noreply.github.com>
2019-10-15 04:14:19 -0400

    Basic Inventory System Integration
    Set up new Scripted Object to contain data for equipment
    Wrote a script to contain a list of all equipment editable via inspector. The script has Save(), Load(), ControlUI(), and Upgrade() Functions blocked out with comments

---
+++ Foundations 1 - Project/Assets/Scripts/EquipmentContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Equipable Item")]
+
+public class EquipmentContainer : ScriptableObject {
+    public GameObject Prefab;//attach model to the scripted object or scripted object to the model?
+    public string ItemName;
+    public string ItemDescription, ItemType;
+
+    public float ItemDurability;
+    public float ItemEnergy;
+    public float ItemPower;
+
+    public int ItemAmmunition;
+
+    public bool PlayerHasItem = false; //by default the player won't have the item, load function will set bools
+
+    public Sprite ItemArtwork;
+
+}
---
+++ Foundations 1 - Project/Assets/Scripts/InfoManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI; //might need UI for giving info to the UI when acsessing this from another script
+
+public class InfoManager : MonoBehaviour {
+    public List<EquipmentContainer> Equipment;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0;i<= 2;i++) {
+            Debug.Log("Item Name: " + Equipment[i].ItemName + "Desc: " + Equipment[i].ItemDescription);
+        }
+       }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Save()
+    {
+        //Save to text file all equipment with bool checked     
+    }
+    void Load()
+    {
+        //Load Data from text file
+    }
+    void ControlUI()
+    {
+        //Give The UI info as needed from the scriptable object
+    }
+    void Upgrade()
+    {
+        //Change the bool of the item as needed to reflect the upgrade
+    }
+}
