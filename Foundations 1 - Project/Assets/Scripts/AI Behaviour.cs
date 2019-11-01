13aa8879ac76740b6957f09fb1bbf15d4287823b
GC - Laptop <gc344@njit.edu>
2019-11-01 18:06:43 -0400

    Post Presentation Commit

--- Foundations 1 - Project/Assets/Scripts/AI Behaviour.cs
+++ Foundations 1 - Project/Assets/Scripts/AI Behaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AIBehaviour : MonoBehaviour {
-    public float Agression;
+    public float agressionLevel;
     
 	
 	void Start () {
@@ -26,6 +26,19 @@ public class AIBehaviour : MonoBehaviour {
      * 
      */
 
+    //roots
+    public void Attacking()
+    {
+
+    }
+    public void Defending()
+    {
+
+    }
+    public void Evading()
+    {
+
+    }
 
     public bool CheckVisible()
     {
@@ -56,13 +69,64 @@ public class AIBehaviour : MonoBehaviour {
     {
 
     }
-    public void Grapple()
+    public void Throw(int i)
+    {
+        if (i == 1)//Throw Them Down
+        {
+            //do animation from throw
+            //playerHP -= (damage - playerArmorValue);
+        }
+        if (i == 2)//Throw them away
+        {
+
+        }
+       
+    }
+    public void Pummle()
+    {
+        //smack player to deal light damage that ignores armor 
+    }
+
+    public void CheckPlayerHP()
     {
 
     }
-    public void ReturnToNeutral()
+    // public void Throw() { }
+    public void MoveBackward()
+    {
+     
+    }
+    public void MoveForward()
     {
 
+    }
+    public void MoveLeft()
+    {
+
+    }
+    public void MoveRight()
+    {
+
+    }
+    //Sequences
+    public void BasicCombo()
+    {
+        LightAttack();
+        LightAttack();
+        HeavyAttack();
+    }
+    public void Grapple()
+
+    {
+        Pummle();
+        Throw(1);
+        //throw
+    }
+    public void ReturnToNeutral()
+    {
+        MoveBackward();
+        CheckVisible();
+        CheckPlayerHP();
     }
     public void EngageCloseCombat()
     {
